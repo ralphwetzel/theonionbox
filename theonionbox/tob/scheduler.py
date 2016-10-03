596d307d910e025ee1205877b4ac4ab64f332c8f
ralphwetzel <theonionbox@gmx.com>
2016-10-03 18:02:54 +0200

    Fixed: Support for APScheduler v3 (as before) and and v2 (new).

--- theonionbox/tob/scheduler.py
+++ theonionbox/tob/scheduler.py
@@ -2,7 +2,6 @@
 # Class to support APScheduler v3 (default) and v2
 # despite their interfaces are incompatible!
 
-
 class Scheduler(object):
 
     schedulr = None
@@ -38,30 +37,30 @@ class Scheduler(object):
 
             return None
 
-    def add_job(self, func, trigger, id=None, *args, **kwargs):
+    def add_job(self, func, trigger, args=None, kwargs=None, id=None, **trigger_args):
         if self.aps30:
-            return self.schedulr.add_job(func, trigger, id=id, args=args, kwargs=kwargs)
+            return self.schedulr.add_job(func, trigger, id=id, args=args, kwargs=kwargs, **trigger_args)
         else:
             if trigger is 'date':
-                run_date = kwargs['run_date']   # by intention: to raise if not set!
-                del kwargs['run_date']
+                run_date = trigger_args['run_date']   # by intention: to raise if not set!
+                del trigger_args['run_date']
                 return self.schedulr.add_date_job(func, run_date, name=id, args=args, kwargs=kwargs)
             elif trigger is 'interval':
                 # only partially implemented!!
                 seconds = 0
                 minutes = 0
                 hours = 0
-                if 'seconds' in kwargs:
-                    seconds = kwargs.get('seconds', 0)
-                    del kwargs['seconds']
+                if 'seconds' in trigger_args:
+                    seconds = trigger_args.get('seconds', 0)
+                    del trigger_args['seconds']
 
-                if 'minutes' in kwargs:
-                    minutes = kwargs.get('minutes', 0)
-                    del kwargs['minutes']
+                if 'minutes' in trigger_args:
+                    minutes = trigger_args.get('minutes', 0)
+                    del trigger_args['minutes']
 
-                if 'hours' in kwargs:
-                    hours = kwargs.get('hours', 0)
-                    del kwargs['hours']
+                if 'hours' in trigger_args:
+                    hours = trigger_args.get('hours', 0)
+                    del trigger_args['hours']
 
                 return self.schedulr.add_interval_job(func, name=id,
                                                       hours=hours, minutes=minutes, seconds=seconds,
@@ -71,17 +70,17 @@ class Scheduler(object):
                 second = 0
                 minute = 0
                 hour = 0
-                if 'second' in kwargs:
-                    second = kwargs.get('second', 0)
-                    del kwargs['second']
+                if 'second' in trigger_args:
+                    second = trigger_args.get('second', 0)
+                    del trigger_args['second']
 
-                if 'minute' in kwargs:
-                    minute = kwargs.get('minute', 0)
-                    del kwargs['minute']
+                if 'minute' in trigger_args:
+                    minute = trigger_args.get('minute', 0)
+                    del trigger_args['minute']
 
-                if 'hour' in kwargs:
-                    hour = kwargs.get('hour', 0)
-                    del kwargs['hour']
+                if 'hour' in trigger_args:
+                    hour = trigger_args.get('hour', 0)
+                    del trigger_args['hour']
 
                 return self.schedulr.add_cron_job(func, name=id, hour=hour, minute=minute, second=second)
             else:
