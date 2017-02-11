dab1e6f864a5e14c80a4002861bec37fbfa2513a
ralphwetzel <theonionbox@gmx.com>
2017-02-11 15:40:05 +0100

    LOCAL Status: Detection of '-rc' versions implemented when verifying Tor version number. Label changed from 'Latest Alpha Version' to 'Latest Unstable Version'.

--- theonionbox/tob/version_tester.py
+++ theonionbox/tob/version_tester.py
@@ -20,7 +20,8 @@ class Version(object):
 class TorVersionTester(object):
 
     stable_list = []
-    alpha_list = []
+    unstable_list = []
+    rc_list = []
 
     def __init__(self):
         self.update()
@@ -30,26 +31,33 @@ class TorVersionTester(object):
         r = requests.get('https://dist.torproject.org/')
 
         if r.status_code == 200:
-            versions = re.findall('(?!tor-)(\d\.\d\.\d{1,2}\.\d{1,2}(?:-alpha)?)(?=\.tar\.gz)', r.text)
+            versions = re.findall('(?!tor-)(\d\.\d\.\d{1,2}\.\d{1,2}(?:-alpha|-rc)?)(?=\.tar\.gz)', r.text)
             versions = list(set(versions))  # remove all duplicates
 
             self.stable_list = []
-            self.alpha_list = []
+            self.unstable_list = []
+            self.rc_list = []
 
             for version in versions:
                 alpha = False
+                rc = False
                 if version[-6:] == '-alpha':
                     alpha = True
                     version = version[:-6]
+                elif version[-3:] == '-rc':
+                    rc = True
+                    version = version[:-3]
                 v = version.split('.')      # separate the version number parts
                 v = [int(y) for y in v]     # string to int
-                if alpha:
-                    self.alpha_list.append(v)
+                if alpha or rc:
+                    self.unstable_list.append(v)
                 else:
                     self.stable_list.append(v)
+                if rc:
+                    self.rc_list.append(v)
 
             self.stable_list.sort()   # sort; latest version will be last entry
-            self.alpha_list.sort()
+            self.unstable_list.sort()
 
             return True
 
@@ -59,9 +67,13 @@ class TorVersionTester(object):
         if len(self.stable_list) > 0:
             return '.'.join(str(i) for i in self.stable_list[-1])
 
-    def latest_alpha(self):
-        if len(self.alpha_list) > 0:
-            return '{}-alpha'.format('.'.join(str(i) for i in self.alpha_list[-1]))
+    def latest_unstable(self):
+        if len(self.unstable_list) > 0:
+            v = self.unstable_list[-1]
+            pf = 'alpha'   # postfix
+            if v in self.rc_list:
+                pf = 'rc'
+            return '{}-{}'.format('.'.join(str(i) for i in v), pf)
 
     def latest(self):
         ls = la = [0, 0, 0, 0]
@@ -69,13 +81,13 @@ class TorVersionTester(object):
         if len(self.stable_list) > 0:
             ls = self.stable_list[-1]
 
-        if len(self.alpha_list) > 0:
-            la = self.alpha_list[-1]
+        if len(self.unstable_list) > 0:
+            la = self.unstable_list[-1]
 
         if ls > la:
             return self.latest_stable()
         else:
-            return self.latest_alpha()
+            return self.latest_unstable()
 
     def is_latest(self, version):
         return str(version) == self.latest()
@@ -83,46 +95,16 @@ class TorVersionTester(object):
     def is_latest_stable(self, version):
         return str(version) == self.latest_stable()
 
-    def is_latest_alpha(self, version):
-        return str(version) == self.latest_alpha()
+    def is_latest_unstable(self, version):
+        return str(version) == self.latest_unstable()
 
     @staticmethod
     def test_alpha(self, version):
         v = str(version)
         return v[-6:] == '-alpha'
 
-class tobVersionTester(object):
-
-    latest = (0, 0, 0)
-
-    def __init__(self):
-        self.update()
-
-    def update(self):
-
-        r = requests.get('https://api.github.com/repos/ralphwetzel/theonionbox/releases/latest')
-
-        if r.status_code == 200:
-            json = r.json()
-
-            tn = json.get('tag_name', None)
-            if tn:
-                if tn[0] == 'v':
-                    tn = tn[1:]
-
-                v = tn.split('.')
-                v = [int(y) for y in v]  # string to int
-                while len(v) < 3:
-                    v.append(0)
-
-                latest = tuple(v[:2])
-
-                return True
-
-        return False
-
-    def is_latest_release(self, version):
-        pass
-
-
+    @staticmethod
+    def test_rc(self, version):
+        v = str(version)
+        return v[-3:] == '-rc'
 
