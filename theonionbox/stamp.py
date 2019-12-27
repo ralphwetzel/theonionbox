d34a2a581951e8fd4b7fc2fb492307ccd276966c
ralphwetzel <theonionbox@gmx.com>
2019-12-27 19:53:02 +0100

    Fix to prevent setup.py from import theonionbox; addressing PEP#517.

--- theonionbox/stamp.py
+++ theonionbox/stamp.py
@@ -1,4 +1,4 @@
 __title__ = 'The Onion Box'
 __description__ = 'Dashboard to monitor Tor node operations.'
-__version__ = '19.2'
-__stamp__ = '20191202|224123'
+__version__ = '19.2.post4'
+__stamp__ = '20191227|195009'
