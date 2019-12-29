4ac777f7ee5d498ec0c73e1c9fe1b20409b5bf08
ralphwetzel <theonionbox@gmx.com>
2019-12-29 23:31:10 +0100

    19.3

--- theonionbox/stamp.py
+++ theonionbox/stamp.py
@@ -1,4 +1,4 @@
 __title__ = 'The Onion Box'
 __description__ = 'Dashboard to monitor Tor node operations.'
-__version__ = '19.3b1'
-__stamp__ = '20191227|201158'
+__version__ = '19.3'
+__stamp__ = '20191229|232641'
