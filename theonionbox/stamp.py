94502e8ce491d4edb28c057d47fb149e04818661
ralphwetzel <theonionbox@gmx.com>
2020-01-18 22:28:09 +0100

    20.2: Fix for #73

--- theonionbox/stamp.py
+++ theonionbox/stamp.py
@@ -1,4 +1,4 @@
 __title__ = 'The Onion Box'
 __description__ = 'Dashboard to monitor Tor node operations.'
-__version__ = '20.1'
-__stamp__ = '20200105|230447'
+__version__ = '20.2'
+__stamp__ = '20200118|222159'
